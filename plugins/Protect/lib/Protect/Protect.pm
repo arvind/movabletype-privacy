84226a33938d93490cd0d3b9fc260e85ea3302ea
Arvind Satyanarayan <arvind.2111@gmail.com>
2005-05-10 12:53:59 +0000

    Created install subrountine for Typekey PHP files

--- plugins/Protect/lib/Protect/Protect.pm
+++ plugins/Protect/lib/Protect/Protect.pm
@@ -55,13 +55,34 @@ sub init
     $app;
 }
 
+sub build_page {
+
+    my $app = shift;
+    my $q = $app->{query};
+    my($page, $param) = @_;
+    $param->{plugin_name       } =  "Protect";
+    $param->{plugin_version    } =  $VERSION;
+    $param->{plugin_author     } =  "Arvind Satyanarayan";
+    $param->{mtscript_url      } =  $app->{mtscript_url};
+    $param->{script_full_url   } =  $app->base . $app->uri;
+    $param->{mt_version        } =  MT->VERSION;
+    $param->{language_tag      } =  $app->current_language;
+    $param->{author_name       } =  $app->{author}->name;
+    $app->SUPER::build_page($page, $param);
+  }
+
 sub config_global {
     my $app = shift;
     my $q = $app->{query};
     my $param;
-
-    
-    $app->add_breadcrumb("MT Protect",$app->{uri});
+		my $cblog_id = $q->param('cblog_id');
+    if($cblog_id){
+    	my $cblog = MT::Blog->load($cblog_id); 
+    $param->{cblog} = $cblog->name;
+    $param->{installed} = $q->param('installed');
+    $param->{uninstalled} = $q->param('uninstalled');
+  }
+    $app->add_breadcrumb("MT Protect",'mt-protect.cgi?__mode=global_config');
     $app->add_breadcrumb("Global Config");
 #    $param->{breadcrumbs} = $app->{breadcrumbs};
 #    $param->{breadcrumbs}[-1]{is_last} = 1;
@@ -81,5 +102,73 @@ sub config_global {
 $app->build_page('config.tmpl', $param);
 }
 
+sub install {
+    my $app = shift;
+    my $q = $app->{query};
+    my $blog_id = $q->param('cblog_id');	
+    my $type = $q->param('_type');
+    my $blog = MT::Blog->load($blog_id);
+    my $auth_typekey = $blog->site_path . "/Auth_TypeKey.php";
+    my $typekey_lib = $blog->site_path . "/typekey_lib.php";
+    if($type eq 'install') {
+    	my $url = 'http://www.movalog.com/archives/plugins/protect/Auth_Typekey.txt';
+	my $auth_tk_text = _load_link ( $url );
+
+	    if (open(TARGET, ">$auth_typekey")) {
+                print TARGET $auth_tk_text;
+                close TARGET;
+            } else {
+                die;
+            }
+
+    $url = 'http://www.movalog.com/archives/plugins/protect/typekey_lib.txt';
+		$auth_tk_text = _load_link ( $url );
+		
+			    if (open(TARGET, ">$typekey_lib")) {
+		                print TARGET $auth_tk_text;
+		                close TARGET;
+		            } else {
+		                die;
+		            }
+		my $data = MT::PluginData->new;
+    $data->plugin('Protect');   
+    $data->key($blog_id);
+    $data->data({ status => "Installed" });
+    $data->save or
+        return $app->error("Error: " . $data->errstr); 
+    $q->param('installed', 1);           
+	} elsif($type eq 'uninstall') {
+			if (-f $auth_typekey) {
+					if (unlink $auth_typekey) {
+						$app->log("Deleted Auth_Typeky.php");
+					}
+			}
+			if (-f $typekey_lib) {
+					if (unlink $typekey_lib) {
+						$app->log("Deleted Auth_Typeky.php");
+					}
+			}
+			my $pdblog = MT::PluginData->load({ plugin => 'Protect', key    => $blog_id });	
+			$pdblog->remove; 
+			$q->param('uninstalled', 1);			 
+		}   
+config_global($app);
+}
+
+#####################################################################
+# UTILITY SUBROUTINES
+#####################################################################
+
+sub _load_link {
+    my $link = shift;
+    require LWP::UserAgent;
+    my $ua = LWP::UserAgent->new;
+    my $req = HTTP::Request->new ( GET => $link );
+    $ua->timeout (15);
+    $ua->agent( "MTBlogroll/$VERSION" );
+    my $result = $ua->request( $req );
+    return '' unless $result->is_success;
+    return $result->content;
+}
 
 1;
