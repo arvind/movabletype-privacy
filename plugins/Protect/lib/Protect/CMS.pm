f28404b363d76737d6f148f6d7990631758542e4
Arvind Satyanarayan <arvind.2111@gmail.com>
2005-05-11 15:06:21 +0000

    subrountine save for entry added. Protect::Protect->data automatically serializes now.

--- plugins/Protect/lib/Protect/CMS.pm
+++ plugins/Protect/lib/Protect/CMS.pm
@@ -29,6 +29,7 @@ sub init
     'global_config'       => \&config_global,
     'install'             => \&install,
     'edit'                => \&edit,
+    'save'                => \&save,
     );
     
     
@@ -41,7 +42,7 @@ sub init
 }
 
 sub build_page {
-
+    
     my $app = shift;
     my $q = $app->{query};
     my($page, $param) = @_;
@@ -54,128 +55,178 @@ sub build_page {
     $param->{language_tag      } =  $app->current_language;
     $param->{author_name       } =  $app->{author}->name;
     $app->SUPER::build_page($page, $param);
-  }
+}
 
 sub config_global {
     my $app = shift;
     my $q = $app->{query};
     my $param;
-		my $cblog_id = $q->param('cblog_id');
+    my $cblog_id = $q->param('cblog_id');
     if($cblog_id){
-    	my $cblog = MT::Blog->load($cblog_id); 
-    $param->{cblog} = $cblog->name;
-    $param->{installed} = $q->param('installed');
-    $param->{uninstalled} = $q->param('uninstalled');
-  }
+        my $cblog = MT::Blog->load($cblog_id);
+        $param->{cblog} = $cblog->name;
+        $param->{installed} = $q->param('installed');
+        $param->{uninstalled} = $q->param('uninstalled');
+    }
     $app->add_breadcrumb("MT Protect",'mt-protect.cgi?__mode=global_config');
     $app->add_breadcrumb("Global Config");
-#    $param->{breadcrumbs} = $app->{breadcrumbs};
-#    $param->{breadcrumbs}[-1]{is_last} = 1;
-		if (my $auth = $app->{author}) {
-		        my @perms = MT::Permission->load({ author_id => $auth->id });
-		        my @data;
-		        for my $perms (@perms) {
-		            next unless $perms->role_mask;
-		            my $blog = MT::Blog->load($perms->blog_id);
-		            my $pdblog = MT::PluginData->load({ plugin => 'Protect', key    => $perms->blog_id });
-		            push @data, { blog_id   => $blog->id,
-		            blog_name => $blog->name,
-		            blog_installed => $pdblog };
-		        }
-		        $param->{blog_loop} = \@data;
-		    }
-$app->build_page('config.tmpl', $param);
+    #    $param->{breadcrumbs} = $app->{breadcrumbs};
+    #    $param->{breadcrumbs}[-1]{is_last} = 1;
+if (my $auth = $app->{author}) {
+        my @perms = MT::Permission->load({ author_id => $auth->id });
+        my @data;
+        for my $perms (@perms) {
+            next unless $perms->role_mask;
+            my $blog = MT::Blog->load($perms->blog_id);
+            my $pdblog = MT::PluginData->load({ plugin => 'Protect', key    => $perms->blog_id });
+            push @data, { blog_id   => $blog->id,
+                blog_name => $blog->name,
+            blog_installed => $pdblog };
+        }
+        $param->{blog_loop} = \@data;
+    }
+    $app->build_page('config.tmpl', $param);
 }
 
 sub install {
     my $app = shift;
     my $q = $app->{query};
-    my $blog_id = $q->param('cblog_id');	
+    my $blog_id = $q->param('cblog_id');
     my $type = $q->param('_type');
     my $blog = MT::Blog->load($blog_id);
     my $auth_typekey = $blog->site_path . "/Auth_TypeKey.php";
     my $typekey_lib = $blog->site_path . "/typekey_lib.php";
     if($type eq 'install') {
-    	my $url = 'http://www.movalog.com/archives/plugins/protect/Auth_Typekey.txt';
-	my $auth_tk_text = _load_link ( $url );
-
-	    if (open(TARGET, ">$auth_typekey")) {
-                print TARGET $auth_tk_text;
-                close TARGET;
+        my $url = 'http://www.movalog.com/archives/plugins/protect/Auth_Typekey.txt';
+        my $auth_tk_text = _load_link ( $url );
+        
+        if (open(TARGET, ">$auth_typekey")) {
+            print TARGET $auth_tk_text;
+            close TARGET;
             } else {
-                die;
+            die;
+        }
+        
+        $url = 'http://www.movalog.com/archives/plugins/protect/typekey_lib.txt';
+        $auth_tk_text = _load_link ( $url );
+        
+        if (open(TARGET, ">$typekey_lib")) {
+            print TARGET $auth_tk_text;
+            close TARGET;
+            } else {
+            die;
+        }
+        my $data = MT::PluginData->new;
+        $data->plugin('Protect');
+        $data->key($blog_id);
+        $data->data({ status => "Installed" });
+        $data->save or
+        return $app->error("Error: " . $data->errstr);
+        $q->param('installed', 1);
+        } elsif($type eq 'uninstall') {
+        if (-f $auth_typekey) {
+            if (unlink $auth_typekey) {
+                $app->log("Deleted Auth_Typeky.php");
             }
-
-    $url = 'http://www.movalog.com/archives/plugins/protect/typekey_lib.txt';
-		$auth_tk_text = _load_link ( $url );
-		
-			    if (open(TARGET, ">$typekey_lib")) {
-		                print TARGET $auth_tk_text;
-		                close TARGET;
-		            } else {
-		                die;
-		            }
-		my $data = MT::PluginData->new;
-    $data->plugin('Protect');   
-    $data->key($blog_id);
-    $data->data({ status => "Installed" });
-    $data->save or
-        return $app->error("Error: " . $data->errstr); 
-    $q->param('installed', 1);           
-	} elsif($type eq 'uninstall') {
-			if (-f $auth_typekey) {
-					if (unlink $auth_typekey) {
-						$app->log("Deleted Auth_Typeky.php");
-					}
-			}
-			if (-f $typekey_lib) {
-					if (unlink $typekey_lib) {
-						$app->log("Deleted Auth_Typeky.php");
-					}
-			}
-			my $pdblog = MT::PluginData->load({ plugin => 'Protect', key    => $blog_id });	
-			$pdblog->remove; 
-			$q->param('uninstalled', 1);			 
-		}   
-config_global($app);
+        }
+        if (-f $typekey_lib) {
+            if (unlink $typekey_lib) {
+                $app->log("Deleted Auth_Typeky.php");
+            }
+        }
+        my $pdblog = MT::PluginData->load({ plugin => 'Protect', key    => $blog_id });
+        $pdblog->remove;
+        $q->param('uninstalled', 1);
+    }
+    config_global($app);
 }
 
 sub edit {
-	my $app = shift;
-  my $q = $app->{query};
-	my $param;
-	my $tmpl;
-	my @data;
-	my $blog_id = $q->param('blog_id');
-	my $entry_id = $q->param('id');
-	my $blog = MT::Blog->load($blog_id);
-	my $entry = MT::Entry->load($entry_id);
-	$param->{entry_title} = $entry->title;
-	$param->{entry_id} = $entry_id;
-	my $type = $q->param('_type');
-	if($type eq 'entry') {
-		for (my $i = 1; $i <= 5; $i++) {
-		    push @data, $i;
-		}			
-		$tmpl = 'edit_entry.tmpl';
-		my $data = Protect::Protect->load({entry_id    => $entry_id });
-		if($data){
-			my $data_type = $data->type;
-			if($data_type eq 'Password'){
-				$param->{is_password} = 1; 
-				$param->{password} = $data->data->{password};
-			}
-			elsif($data_type eq 'Typekey'){
-				$param->{typekey} = 1;
-			}
-		}
-	 $param->{typekey_user_loop} = \@data;
-	 $app->add_breadcrumb($blog->name,$app->{mtscript_url}.'?__mode=menu&blog_id='.$blog->id);
-	 $app->add_breadcrumb($app->translate('Entries'), $app->{mtscript_url} . '?__mode=list_entries&blog_id=' . $blog_id);
-   $app->add_breadcrumb($entry->title || $app->translate('(untitled)'), $app->{mtscript_url} . '?__mode=view&_type=entry&id=' . $entry_id . '&blog_id=' . $blog_id);	
-   $app->add_breadcrumb("Password Protect");
-	}
-	$app->build_page($tmpl, $param);	
+    my $app = shift;
+    my $q = $app->{query};
+    my $param;
+    my $tmpl;
+    my @data;
+    my $blog_id = $q->param('blog_id');
+    my $entry_id = $q->param('id');
+    my $blog = MT::Blog->load($blog_id);
+    my $entry = MT::Entry->load($entry_id);
+    $param->{entry_title} = $entry->title;
+    $param->{entry_id} = $entry_id;
+    my $type = $q->param('_type');
+    if($type eq 'entry') {
+        $tmpl = 'edit_entry.tmpl';
+        my $data = Protect::Protect->load({entry_id    => $entry_id });
+        if($data){
+            my $data_type = $data->type;
+            if($data_type eq 'Password'){
+                $param->{is_password} = 1;
+                my $password = $data->data;
+                $param->{password} = $$password;
+            }
+            elsif($data_type eq 'Typekey'){
+                $param->{is_typekey} = 1;
+                my $users = $data->data;
+                for my $user (@$users) {
+                    my $row = {
+                        user => $user
+                    };
+                    
+                    push @data, $row;
+                }
+            }
+        }
+        for (my $i = 1; $i <= 5; $i++) {
+            push @data, $i;
+        }
+        $param->{message} = $q->param('message');
+        $param->{typekey_user_loop} = \@data;
+        $app->add_breadcrumb($blog->name,$app->{mtscript_url}.'?__mode=menu&blog_id='.$blog->id);
+        $app->add_breadcrumb($app->translate('Entries'), $app->{mtscript_url} . '?__mode=list_entries&blog_id=' . $blog_id);
+        $app->add_breadcrumb($entry->title || $app->translate('(untitled)'), $app->{mtscript_url} . '?__mode=view&_type=entry&id=' . $entry_id . '&blog_id=' . $blog_id);
+        $app->add_breadcrumb("Password Protect");
+    }
+    $app->build_page($tmpl, $param);
+}
+
+sub save {
+    my $app = shift;
+    my $q = $app->{query};
+    my $param;
+    my $tmpl;
+    my @data;
+    my $type = $q->param('_type');
+    if($type eq 'entry') {
+        my $blog_id = $q->param('blog_id');
+        my $entry_id = $q->param('id');
+        my $protection = $q->param('protection');
+        my $data;
+    unless($data = Protect::Protect->load({ entry_id   => $entry_id })){
+            $data = Protect::Protect->new;
+            $data->blog_id($blog_id);
+            $data->entry_id($entry_id);
+            $data->created_by($app->{author}->id);
+        }
+        if($protection eq 'Password') {
+            $data->type($protection);
+            my $password = $q->param('password');
+            $data->data(\$password);
+            $q->param('message', 'Entry now password protected');
+            } elsif($protection eq 'Typekey') {
+            $q->param('message', 'Entry now Typekey protected');            	
+            $data->type($protection);
+            my @users;
+            for my $user ($q->param('typekey_users')) {
+                if($user && $user ne ""){
+                    push (@users, $user);
+                }
+            }
+            $data->data(\@users);
+        }
+        $data->save;
+        edit($app);
+    }
+    
 }
 
 #####################################################################
@@ -194,4 +245,10 @@ sub _load_link {
     return $result->content;
 }
 
-1;
+sub debug {
+    my $err = shift;
+    my $mark = shift || '>';
+    print STDERR "$mark $err\n" if $DEBUG;
+}
+
+1; 
--- plugins/Protect/lib/Protect/Protect.pm
+++ plugins/Protect/lib/Protect/Protect.pm
@@ -1,6 +1,8 @@
 package Protect::Protect;
 use strict;
 
+use Storable qw( freeze thaw );
+
 use MT::Object;
 @Protect::Protect::ISA = qw( MT::Object );
 __PACKAGE__->install_properties({
@@ -12,8 +14,17 @@ __PACKAGE__->install_properties({
         entry_id => 1,
         type => 1,
     },
+    column_defs => {
+        data => 'blob',
+    },
     datasource => 'protect',
     primary_key => 'id',
 });
+
+sub data {
+    my $data = shift;
+    $data->column('data', freeze(shift)) if @_;
+    thaw($data->column('data'));
+} 
 
 1;
